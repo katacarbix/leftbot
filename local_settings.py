2aff8014c136fc0a69b0b6d700dc9325732d1932
Reese Jacobson <me@katacarbix.xyz>
2019-08-28 19:52:48 -0500

    ahaha one more error

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,7 @@
 from os import environ
 
 # Configuration for Twitter API
-ENABLE_TWITTER_POSTING = environ.get('ENABLE_TWITTER_POSTING') # Tweet resulting status?
+ENABLE_TWITTER_POSTING = True # Tweet resulting status?
 MY_CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY') # Your Twitter API Consumer Key set in Heroku config
 MY_CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET') # Your Consumer Secret Key set in Heroku config
 MY_ACCESS_TOKEN_KEY = environ.get('TWITTER_ACCESS_TOKEN_KEY') # Your Twitter API Access Token Key set in Heroku config

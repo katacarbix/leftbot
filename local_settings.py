b0a1b84a0233cbc950cfba7713925b5e0229d1c6
Reese Jacobson <me@katacarbix.xyz>
2019-08-28 19:37:38 -0500

    forgot to change testing values

--- local_settings.py
+++ local_settings.py
@@ -1,7 +1,7 @@
 from os import environ
 
 # Configuration for Twitter API
-ENABLE_TWITTER_POSTING = environ.get('ENABLE_TWITTER_POSTING') or False # Tweet resulting status?
+ENABLE_TWITTER_POSTING = environ.get('ENABLE_TWITTER_POSTING') # Tweet resulting status?
 MY_CONSUMER_KEY = environ.get('TWITTER_CONSUMER_KEY') # Your Twitter API Consumer Key set in Heroku config
 MY_CONSUMER_SECRET = environ.get('TWITTER_CONSUMER_SECRET') # Your Consumer Secret Key set in Heroku config
 MY_ACCESS_TOKEN_KEY = environ.get('TWITTER_ACCESS_TOKEN_KEY') # Your Twitter API Access Token Key set in Heroku config
@@ -11,4 +11,4 @@ USERNAME = 'lefttube_ebooks'
 
 GOOGLE_API_KEY = environ.get('GOOGLE_API_KEY') # Google/Youtube API key for scraping titles
 
-ORDER = environ.get('ORDER') or 2 # How closely do you want this to hew to sensical? 2 is low and 4 is high.
+ORDER = environ.get('ORDER') # How closely do you want this to hew to sensical? 2 is low and 4 is high.
